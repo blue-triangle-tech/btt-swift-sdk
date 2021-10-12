89db1df76ad867d4bd29ec5190451a8bebc8f875
Matt Gacy <matt@mgacy.com>
2021-10-11 19:46:02 -0700

    Configuration

--- Sources/BlueTriangle/BlueTriangle.swift
+++ Sources/BlueTriangle/BlueTriangle.swift
@@ -1,6 +1,59 @@
-public struct BlueTriangle {
-    public private(set) var text = "Hello, World!"
+//
+//  BlueTriangle.swift
+//
+//  Created by Mathew Gacy on 10/8/21.
+//  Copyright © 2021 Blue Triangle. All rights reserved.
+//
 
-    public init() {
+import Foundation
+
+final public class BlueTriangleConfiguration: NSObject {
+
+    /// Blue Triangle Technologies-assigned site ID.
+    @objc public var siteID: String = ""
+
+    /// Session ID.
+    @objc public var sessionID: String = "" // `sID`
+
+    /// Global User ID
+    @objc public var globalUserID: String = "" // `gID`
+
+    /// A/B testing identifier.
+    @objc public var abTestID: String = "Default" // `AB`
+
+    /// Legacy campaign name.
+    @available(*, deprecated, message: "Use `campaignName` instead.")
+    @objc public var campaign: String? = "" // `campaign`
+
+    /// Campaign medium.
+    @objc public var campaignMedium: String = "" // `CmpM`
+
+    /// Campaign name.
+    @objc public var campaignName: String = "" // `CmpN`
+
+    /// Campaign source.
+    @objc public var campaignSource: String = "" // `CmpS`
+
+    /// Data center.
+    @objc public var dataCenter: String = "Default" // `DCTR`
+
+    /// Traffic segment.
+    @objc public var trafficSegmentName: String = "" // `txnName`
+}
+
+final public class BlueTriangle: NSObject {
+
+    private static let lock = NSLock()
+    private static var configuration = BlueTriangleConfiguration()
+
+    public private(set) static var initialized = false
+
+    @objc
+    public static func configure(_ configure: (BlueTriangleConfiguration) -> Void) {
+        lock.sync {
+            precondition(!Self.initialized, "BlueTriangle can only be initialized once.")
+            configure(configuration)
+            Self.initialized = true
+        }
     }
 }
---
+++ Sources/BlueTriangle/Extensions/NSLocking+Utils.swift
@@ -0,0 +1,16 @@
+//
+//  NSLocking+Utils.swift
+//
+//  Created by Mathew Gacy on 10/8/21.
+//  Copyright © 2021 Blue Triangle. All rights reserved.
+//
+
+import Foundation
+
+extension NSLocking {
+    func sync<T>(_ closure: () -> T) -> T {
+        lock()
+        defer { unlock() }
+        return closure()
+    }
+}
