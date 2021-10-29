3744c52019a04a33f50324e634be7e825a812714
Matt Gacy <matt@mgacy.com>
2021-10-29 10:59:11 -0700

    Additional testing support

--- Tests/BlueTriangleTests/BlueTriangleTests.swift
+++ Tests/BlueTriangleTests/BlueTriangleTests.swift
@@ -1,7 +1,28 @@
 import XCTest
+import Combine
 @testable import BlueTriangle
 
 final class BlueTriangleTests: XCTestCase {
+
+    static var uploaderQueue: DispatchQueue = Mock.uploaderQueue
+    static var onSendRequest: (Request) -> Void = { _ in }
+
+    override class func setUp() {
+        super.setUp()
+        BlueTriangle.configure { configuration in
+            configuration.uploaderConfiguration = Mock.makeUploaderConfiguration(queue: uploaderQueue) { request in
+                onSendRequest(request)
+            }
+        }
+        BlueTriangle.prime()
+        BlueTriangle.reset()
+    }
+    
+    override func tearDown() {
+        Self.onSendRequest = { _ in }
+        BlueTriangle.reset()
+        super.tearDown()
+    }
 
     func testOSInfo() {
         let os = Device.os
