3f6f17ea39ad2a4d853c7dcc951cb23216bb9702
Matt Gacy <matt@mgacy.com>
2022-02-03 14:23:34 -0700

    Implement Timer-based PerformanceMonitor

--- Sources/BlueTriangle/Vitals/TimerPerformanceMonitor.swift
+++ Sources/BlueTriangle/Vitals/TimerPerformanceMonitor.swift
@@ -8,27 +8,56 @@
 import Foundation
 import Combine
 
-class TimerPerformanceMonitor: PerformanceMonitoring {
+final class TimerPerformanceMonitor: PerformanceMonitoring {
+    enum State {
+        case initial
+        case started
+        case ended
+    }
+
     private let sampleInterval: TimeInterval
     private let resourceUsage: ResourceUsageMeasuring.Type
+    private let runLoop: RunLoop
+    private let mode: RunLoop.Mode
+    private var cancellable: AnyCancellable?
 
     private(set) var measurements: [ResourceUsageMeasurement] = []
+    private(set) var state: State = .initial
 
-    init(sampleInterval: TimeInterval, resourceUsage: ResourceUsageMeasuring.Type) {
+    init(
+        sampleInterval: TimeInterval,
+        resourceUsage: ResourceUsageMeasuring.Type,
+        runLoop: RunLoop = .current,
+        mode: RunLoop.Mode = .common
+    ) {
         self.sampleInterval = sampleInterval
         self.resourceUsage = resourceUsage
+        self.runLoop = runLoop
+        self.mode = mode
     }
 
     func start() {
-        // ...
+        if state == .started {
+            return
+        }
+
+        cancellable = Timer.publish(every: sampleInterval, on: runLoop, in: mode)
+            .autoconnect()
+            .sink() { [weak self] _ in
+                self?.sample()
+            }
+        sample()
+        state = .started
     }
 
     func end() {
-        // ...
+        cancellable?.cancel()
+        sample()
+        state = .ended
     }
 
     func makeReport() -> PerformanceReport {
-        .init(minCPU: 0.0, maxCPU: 0.0, avgCPU: 0.0, minMemory: 0, maxMemory: 0, avgMemory: 0)
+        measurements.makeReport() ?? .empty
     }
 
     // MARK: - Private
