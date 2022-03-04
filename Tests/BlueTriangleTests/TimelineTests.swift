119624fb9c5644bb6d6ab4cdb6c64a206c3a956a
Matt Gacy <matt@mgacy.com>
2022-03-04 08:56:58 -0700

    Additional Timeline tests

--- Tests/BlueTriangleTests/TimelineTests.swift
+++ Tests/BlueTriangleTests/TimelineTests.swift
@@ -20,8 +20,127 @@ final class TimelineTests: XCTestCase {
     override func setUp() {
         Self.timeIntervals = timeIntervals
     }
+
+    func testInsert() throws {
+        var timeline = Timeline<RequestSpan>(capacity: 5, intervalProvider: Self.timeIntervalProvider)
+        let requestSpan = RequestSpan(Mock.page)
+        timeline.insert(requestSpan)
+        XCTAssertEqual(timeline.current, requestSpan)
+    }
+
+    func testCount() throws {
+        var timeline = Timeline<RequestSpan>(capacity: 5, intervalProvider: Self.timeIntervalProvider)
+        let requestSpan = RequestSpan(Mock.page)
+        XCTAssertEqual(timeline.count, 0)
+        timeline.insert(requestSpan)
+        XCTAssertEqual(timeline.count, 1)
+    }
+
+    func testValueForIfEmpty() throws {
+        let timeline = Timeline<RequestSpan>(capacity: 5)
+        let value = timeline.value(for: 1000)
+        XCTAssertNil(value)
+    }
+
+    func testValueForBefore() throws {
+        var timeline = Timeline<RequestSpan>(capacity: 5, intervalProvider: Self.timeIntervalProvider)
+        timeline.insert(RequestSpan(Mock.page))
+        let value = timeline.value(for: 999)
+        XCTAssertNil(value)
+    }
+
+    func testValueFor() throws {
+        var timeline = Timeline<RequestSpan>(capacity: 5, intervalProvider: Self.timeIntervalProvider)
+        timeline.insert(RequestSpan(Mock.page))
+        let value = timeline.value(for: 1000)!
+        XCTAssertEqual(value.page, Mock.page)
+    }
+
+    func testValueForAfter() throws {
+        var timeline = Timeline<RequestSpan>(capacity: 5, intervalProvider: Self.timeIntervalProvider)
+        timeline.insert(RequestSpan(Mock.page))
+        let value = timeline.value(for: 2000)!
+        XCTAssertEqual(value.page, Mock.page)
+    }
+
+    func testCapacity() throws {
+        let capacity = 4
+        var timeline = Timeline<RequestSpan>(capacity: capacity, intervalProvider: Self.timeIntervalProvider)
+        // Insert spans to reach capacity
+        Array(0...3).map { RequestSpan(Page(pageName: pageNames[$0])) }.forEach { timeline.insert($0) }
+
+        // Insert additional span
+        let popped = timeline.insert(RequestSpan(Page(pageName: pageNames.last!)))!
+        XCTAssertEqual(timeline.count, capacity)
+        XCTAssertEqual(popped.page.pageName, pageNames.first!)
+    }
+
+    func testUpdateValue() throws {
+        var timeline = Timeline<RequestSpan>(capacity: 5, intervalProvider: Self.timeIntervalProvider)
+        let page1 = Page(pageName: pageNames[0])
+        timeline.insert(RequestSpan(page1))
+        let page2 = Page(pageName: pageNames[1])
+        timeline.insert(RequestSpan(page2))
+
+        timeline.updateValue(for: 1050) { $0.insert(Mock.capturedRequest) }
+        let updated = timeline.pop()!
+
+        XCTAssertEqual(updated.page, page1)
+        XCTAssertEqual(updated.requests, [Mock.capturedRequest])
+    }
+
+    func testUpdateCurrent() throws {
+        var timeline = Timeline<RequestSpan>(capacity: 5, intervalProvider: Self.timeIntervalProvider)
+        timeline.insert(RequestSpan(Mock.page))
+
+        timeline.updateCurrent { span in
+            span.insert(Mock.capturedRequest)
+        }
+
+        let updated = timeline.current!
+        XCTAssertEqual(updated.page, Mock.page)
+        XCTAssertEqual(updated.requests, [Mock.capturedRequest])
+    }
+
+    func testPop() throws {
+        var timeline = Timeline<RequestSpan>(capacity: 5, intervalProvider: Self.timeIntervalProvider)
+        pageNames.map { RequestSpan(Page(pageName: $0)) }.forEach { timeline.insert($0) }
+
+        let firstPopped = timeline.pop()
+        XCTAssertEqual(firstPopped?.page.pageName, "page_1")
+        XCTAssertEqual(timeline.count, 4)
+
+        for _ in 0..<4 { timeline.pop() }
+
+        XCTAssertEqual(timeline.count, 0)
+        XCTAssertNil(timeline.current)
+    }
+
+    func testNodeFor() throws {
+        var timeline = Timeline<RequestSpan>(capacity: 5, intervalProvider: Self.timeIntervalProvider)
+        pageNames.map { RequestSpan(Page(pageName: $0)) }.forEach { timeline.insert($0) }
+
+        let nodeBeforeHead = timeline.value(for: 999)
+        XCTAssertNil(nodeBeforeHead)
+
+        let nodeAtHead = timeline.value(for: 1000)
+        XCTAssertEqual(nodeAtHead?.page.pageName, "page_1")
+
+        let nodeAfterHead = timeline.value(for: 1001)
+        XCTAssertEqual(nodeAfterHead?.page.pageName, "page_1")
+
+        let nodeBeforeTail = timeline.value(for: 4999)
+        XCTAssertEqual(nodeBeforeTail?.page.pageName, "page_4")
+
+        let nodeAtTail = timeline.value(for: 5001)
+        XCTAssertEqual(nodeAtTail?.page.pageName, "page_5")
+
+        let nodeAfterTail = timeline.value(for: 5001)
+        XCTAssertEqual(nodeAfterTail?.page.pageName, "page_5")
+    }
 }
 
+// MARK: - Performance
 extension TimelineTests {
     /// MacBook Pro (13-inch, M1, 2020) - macOS 11.6.4 - 1.029 sec for 100_000 pages
     /// average: 1.029
