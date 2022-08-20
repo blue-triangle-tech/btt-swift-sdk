e6813ce1d07811439c6d11f6951daa132d772b5a
Matt Gacy <matt@mgacy.com>
2022-08-20 15:16:52 -0700

    Client for JSONPlaceholder

---
+++ Examples/Example-UIKit/Example-UIKit/Protocols/PlaceholderClientProtocol.swift
@@ -0,0 +1,14 @@
+//
+//  PlaceholderClientProtocol.swift
+//  Example-UIKit
+//
+//  Created by Mathew Gacy on 8/20/22.
+//
+
+import Foundation
+
+protocol PlaceholderClientProtocol {
+    func fetchAlbums() async throws -> [Album]
+    func fetchPhotos(albumId: Int) async throws -> [Photo]
+    func fetchPhoto(url: URL) async throws -> Data
+}
---
+++ Examples/Example-UIKit/Example-UIKit/Services/PlaceholderClient.swift
@@ -0,0 +1,32 @@
+//
+//  PlaceholderClient.swift
+//  Example-UIKit
+//
+//  Created by Mathew Gacy on 8/20/22.
+//
+
+import Foundation
+
+struct PlaceholderClient: PlaceholderClientProtocol {
+    let session: URLSession
+
+    init(session: URLSession) {
+        self.session = session
+    }
+
+    func fetchAlbums() async throws -> [Album] {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/albums")!
+        let data = try await session.btData(from: url)
+        return try JSONDecoder().decode(Array<Album>.self, from: data.0)
+    }
+
+    func fetchPhotos(albumId: Int) async throws -> [Photo] {
+        let url = URL(string: "https://jsonplaceholder.typicode.com/albums/\(albumId)/photos")!
+        let data = try await session.btData(from: url)
+        return try JSONDecoder().decode(Array<Photo>.self, from: data.0)
+    }
+
+    func fetchPhoto(url: URL) async throws -> Data {
+        try await session.btData(from: url).0
+    }
+}
