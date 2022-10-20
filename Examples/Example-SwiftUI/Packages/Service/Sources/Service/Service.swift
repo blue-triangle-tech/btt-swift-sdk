2ece5f3a6eba116877125635ca9d36c728679f67
Matt Gacy <matt@mgacy.com>
2022-10-19 18:46:53 -0700

    Add package

---
+++ Examples/Example-SwiftUI/Packages/Service/Sources/Service/Service.swift
@@ -0,0 +1,13 @@
+//
+//  NetworkError.swift
+//
+//  Created by Mathew Gacy on 10/1/22.
+//  Copyright © 2022 Blue Triangle. All rights reserved.
+//
+
+public struct Service {
+    public private(set) var text = "Hello, World!"
+
+    public init() {
+    }
+}
