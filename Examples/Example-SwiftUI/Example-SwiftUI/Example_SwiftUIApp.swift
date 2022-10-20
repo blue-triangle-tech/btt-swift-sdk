fff9a64eabc369437d32329d255e95e23313d6b9
Matt Gacy <matt@mgacy.com>
2022-10-19 18:31:37 -0700

    Initial commit

---
+++ Examples/Example-SwiftUI/Example-SwiftUI/Example_SwiftUIApp.swift
@@ -0,0 +1,18 @@
+//
+//  Example_SwiftUIApp.swift
+//  Example-SwiftUI
+//
+//  Created by Mathew Gacy on 10/19/22.
+//  Copyright © 2022 Blue Triangle. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct Example_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
