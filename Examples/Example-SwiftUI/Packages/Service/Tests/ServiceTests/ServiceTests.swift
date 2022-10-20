b0a3b425ba2b13b90db20b9982d41062fd45ebc9
Matt Gacy <matt@mgacy.com>
2022-10-19 19:34:07 -0700

    Fix broken test

--- Examples/Example-SwiftUI/Packages/Service/Tests/ServiceTests/ServiceTests.swift
+++ Examples/Example-SwiftUI/Packages/Service/Tests/ServiceTests/ServiceTests.swift
@@ -13,6 +13,5 @@ final class ServiceTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(Service().text, "Hello, World!")
     }
 }
