6a33154c7c18426aafb1294514121cc15d859eff
Matt Gacy <matt@mgacy.com>
2022-11-18 07:44:43 -0700

    Implement CartRepository

--- Examples/Example-SwiftUI/Example-SwiftUI/CartRepository.swift
+++ Examples/Example-SwiftUI/Example-SwiftUI/CartRepository.swift
@@ -42,30 +42,122 @@ final class CartRepository {
 
     @MainActor
     func add(product: Product, quantity: Int) {
+        Task {
+            do {
+                if let item = try await addProduct(product, quantity: quantity) {
+                    var currentItems = items.value
+                    currentItems[id: item.id] = item
+                    items.value = currentItems
+                }
+            } catch {
+                print("ERROR: \(error)")
+            }
+        }
     }
 
     @MainActor
     func updateQuantity(cartItemID: CartItem.ID, quantity: Int) {
+        guard let cartID = cartDetail?.id, let currentItem = items.value[id: cartItemID] else {
+            return
+        }
+
+        Task {
+            do {
+                let updatedItem = try await updateItem(
+                    cartItemID: cartItemID,
+                    product: currentItem.product,
+                    quantity: quantity)
+
+                self.cartDetail = try await service.cart(id: cartID)
+
+                items.value[id: cartItemID] = updatedItem
+
+            } catch {
+                print("ERROR: \(error)")
+            }
+        }
     }
 
     @MainActor
     func remove(cartItemID: CartItem.ID) {
-    }
-}
+        guard let cartID = cartDetail?.id else {
+            return
+        }
 
-extension CartRepository {
-    func add(_ product: Product, quantity: Int = 1) async throws -> CartProductItem? {
+        Task {
+            do {
+                try await service.deleteCartItem(id: cartItemID)
+                items.value[id: cartItemID] = nil
+                cartDetail = try await service.cart(id: cartID)
+            } catch {
+                print("ERROR: \(error)")
+            }
+        }
     }
 }
 
 private extension CartRepository {
     func makeCart() async throws -> Cart {
+        try await service.createCart(
+            CreateCart(
+                confirmation: UUID().uuidString,
+                shipping: Constants.shipping,
+                itemIDs: []))
+    }
+
+    func addProduct(_ product: Product, quantity: Int = 1) async throws -> CartProductItem? {
+        if let cartDetail {
+            if let existingItem = cartDetail.items.first(where: { $0.productID == product.id }) {
+                guard quantity != existingItem.quantity else {
+                    return nil
+                }
+
+                let productItem = try await updateItem(cartItemID: existingItem.id, product: product, quantity: quantity)
+                self.cartDetail = try await service.cart(id: cartDetail.id)
+                return productItem
+
+            } else {
+                let productItem = try await addItem(product: product, quantity: quantity, cartID: cartDetail.id)
+                self.cartDetail = try await service.cart(id: cartDetail.id)
+                return productItem
+            }
+        } else {
+            let newCart = try await service.createCart(
+                CreateCart(
+                    confirmation: UUID().uuidString,
+                    shipping: Constants.shipping))
+
+            let productItem = try await addItem(product: product, quantity: quantity, cartID: newCart.id)
+            cartDetail = try await service.cart(id: newCart.id)
+            return productItem
+        }
     }
 
     func addItem(product: Product, quantity: Int, cartID: Cart.ID) async throws -> CartProductItem {
+        let cartItem = try await service.createCartItem(
+            CreateCartItem(
+                productID: product.id,
+                quantity: quantity,
+                price: product.price,
+                cartID: cartID))
+
+        return CartProductItem(
+            id: cartItem.id,
+            quantity: cartItem.quantity,
+            product: product)
     }
 
     func updateItem(cartItemID: CartItem.ID, product: Product, quantity: Int) async throws -> CartProductItem {
+        let cartItem = try await service.updateCartItem(
+            UpdateCartItem(
+                id: cartItemID,
+                quantity: quantity))
+
+        return CartProductItem(
+            id: cartItem.id,
+            quantity: cartItem.quantity,
+            product: product)
+    }
 }
 
 extension CartRepository {
