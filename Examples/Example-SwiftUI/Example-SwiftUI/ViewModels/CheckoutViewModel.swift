6cdf2acbb3cd84b4175afbbcfe958fd2865ef184
Matt Gacy <matt@mgacy.com>
2022-12-01 11:07:34 -0700

    Implement checkout view model

--- Examples/Example-SwiftUI/Example-SwiftUI/ViewModels/CheckoutViewModel.swift
+++ Examples/Example-SwiftUI/Example-SwiftUI/ViewModels/CheckoutViewModel.swift
@@ -12,6 +12,31 @@ final class CheckoutViewModel: ObservableObject {
     private let cartRepository: CartRepository
     private let onFinish: () -> Void
     @Published var checkout: Checkout
+    @Published var error: Error?
+
+    var estimatedTax: Double {
+        Constants.averageSalesTax * itemTotal
+    }
+
+    var itemCount: Int {
+        checkout.items.reduce(into: 0) { count, item in
+            count += item.quantity
+        }
+    }
+
+    var itemTotal: Double {
+        checkout.items.reduce(into: 0) { result, item in
+            result += Double(item.price).flatMap { $0 * Double(item.quantity) } ?? 0
+        }
+    }
+
+    var shipping: Double {
+        0.0
+    }
+
+    var total: Double {
+        estimatedTax + shipping + itemTotal
+    }
 
     init(
         cartRepository: CartRepository,
@@ -23,7 +48,12 @@ final class CheckoutViewModel: ObservableObject {
         self.onFinish = onFinish
     }
 
-    func placeOrder() {
-        onFinish()
+    func placeOrder() async {
+        do {
+            _ = try await cartRepository.confirm(checkout.id)
+            onFinish()
+        } catch {
+            self.error = error
+        }
     }
 }
