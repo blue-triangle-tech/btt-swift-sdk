441bf288e88503d499ebd9fc95d9e60ad79d68fe
Matt Gacy <matt@mgacy.com>
2022-12-12 11:19:05 -0700

    Update RemoteImage, ProductCell

--- Examples/Example-SwiftUI/Example-SwiftUI/Views/Components/RemoteImage.swift
+++ Examples/Example-SwiftUI/Example-SwiftUI/Views/Components/RemoteImage.swift
@@ -8,25 +8,41 @@
 import SwiftUI
 
 struct RemoteImage: View {
+    @State var imageResult: Result<UIImage, Error>?
     let contentMode: ContentMode = .fit
-    let imageStatus: ImageStatus?
+    let imageStatus: ImageStatus
 
     var body: some View {
-        switch imageStatus {
-        case .downloaded(let uiImage):
-            Image(uiImage: uiImage)
-                .resizable()
-                .aspectRatio(contentMode: contentMode)
-                .cornerRadius(8)
-        case .error:
-            Image(systemName: "exclamationmark.circle")
-                .resizable()
-                .aspectRatio(contentMode: contentMode)
-                .foregroundColor(.red)
-                .padding()
-        default:
-            ProgressView()
-                .padding()
+        Group {
+            if let result = imageResult {
+                switch result {
+                case .success(let uiImage):
+                    Image(uiImage: uiImage)
+                        .resizable()
+                        .aspectRatio(contentMode: contentMode)
+                case .failure:
+                    Image(systemName: "exclamationmark.circle")
+                        .resizable()
+                        .aspectRatio(contentMode: contentMode)
+                        .foregroundColor(.red)
+                        .padding()
+                }
+            } else {
+                ProgressView()
+                    .padding()
+            }
+        }
+        .task {
+            await load(imageStatus)
+        }}
+
+    func load(_ status: ImageStatus) async {
+        switch status {
+        case .loading(let task):
+            imageResult = await task.value.result
+
+        case .downloaded(let result):
+            imageResult = result
         }
     }
 }
