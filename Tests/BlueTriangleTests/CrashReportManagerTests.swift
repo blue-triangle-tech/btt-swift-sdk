df86f01d91316ae9a9a83d8a7d2230119502d3e5
Matt Gacy <matt@mgacy.com>
2023-03-29 14:28:14 -0700

    Add additional tests

--- Tests/BlueTriangleTests/CrashReportManagerTests.swift
+++ Tests/BlueTriangleTests/CrashReportManagerTests.swift
@@ -9,6 +9,10 @@ import XCTest
 @testable import BlueTriangle
 
 final class CrashReportManagerTests: XCTestCase {
+    struct TestError: Error {
+        let message = "There as an error"
+    }
+
     let crashErrorReport = ErrorReport(message: "crash_message", line: 1, column: 2, time: 100)
 
     var crashReport: CrashReport {
@@ -103,5 +107,70 @@ final class CrashReportManagerTests: XCTestCase {
 
         XCTAssertEqual(actualReport.message, crashErrorReport.message)
         XCTAssertEqual(actualReport.time, crashErrorReport.time)
+    }
+
+    func testErrorTimerUploaded() throws {
+        let expectedErrorStart: TimeInterval = 1000.0
+
+        var timerRequest: Request!
+        let uploadExpectation = expectation(description: "Timer uploaded")
+        let uploader = UploaderMock { request in
+            if timerRequest == nil {
+                timerRequest = request
+                uploadExpectation.fulfill()
+            }
+        }
+
+        let sut = CrashReportManager(
+            crashReportPersistence: CrashReportPersistenceMock.self,
+            logger: LoggerMock(),
+            uploader: uploader,
+            sessionProvider: { Mock.session },
+            intervalProvider: { expectedErrorStart }
+        )
+
+        sut.uploadError(TestError(), file: #file, function: #function, line: #line)
+        wait(for: [uploadExpectation], timeout: 1.0)
+
+        let actualTimer = try JSONDecoder().decode(TimerRequest.self, from: timerRequest.body!.base64DecodedData()!)
+        XCTAssertEqual(actualTimer.session.sessionID, Mock.sessionID)
+        XCTAssertEqual(actualTimer.timer.startTime, expectedErrorStart.milliseconds)
+    }
+
+    func testErrorReportUploaded() throws {
+        let expectedErrorStart: TimeInterval = 1000.0
+        let expectedMessage = """
+        TestError(message: "There as an error")
+        """
+
+        var requestCount = 0
+        var errorRequest: Request!
+        let uploadExpectation = expectation(description: "Report uploaded")
+        let uploader = UploaderMock { request in
+            requestCount += 1
+            if requestCount > 1 {
+                errorRequest = request
+                uploadExpectation.fulfill()
+            }
+        }
+
+        let sut = CrashReportManager(
+            crashReportPersistence: CrashReportPersistenceMock.self,
+            logger: LoggerMock(),
+            uploader: uploader,
+            sessionProvider: { Mock.session },
+            intervalProvider: { expectedErrorStart }
+        )
+
+        sut.uploadError(TestError(), file: #file, function: #function, line: #line)
+        wait(for: [uploadExpectation], timeout: 1.0)
+
+        let actualReport = try JSONDecoder().decode([ErrorReport].self,from: errorRequest.body!.base64DecodedData()!).first!
+
+        XCTAssertEqual(errorRequest.parameters!["nStart"], "\(expectedErrorStart.milliseconds)")
+        XCTAssertEqual(errorRequest.parameters!["sessionID"], "\(Mock.sessionID)")
+
+        XCTAssertEqual(actualReport.message, expectedMessage)
+        XCTAssertEqual(actualReport.time, expectedErrorStart.milliseconds)
     }
 }
