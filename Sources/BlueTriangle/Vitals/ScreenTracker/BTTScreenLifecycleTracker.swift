49974c451eb149d87e92ccb71cfa6b26fd913f14
JP-aloha <jaib@alohatechnology.com>
2026-01-28 16:45:18 +0530

    Added page type in constants

--- Sources/BlueTriangle/Vitals/ScreenTracker/BTTScreenLifecycleTracker.swift
+++ Sources/BlueTriangle/Vitals/ScreenTracker/BTTScreenLifecycleTracker.swift
@@ -185,9 +185,9 @@ class TimerMapActivity {
         
         if BlueTriangle.configuration.enableGrouping {
             BlueTriangle.groupTimer.startGroupIfNeeded(pageTitle.isEmpty ? pageName : pageTitle)
-            self.timer = BlueTriangle.startTimer(page:Page(pageName: pageName, pageTitle: pageTitle, pageType: Constants.SCREEN_TRACKING_TRAFFIC_SEGMENT), timerType: .custom, isGroupedTimer: true)
+            self.timer = BlueTriangle.startTimer(page:Page(pageName: pageName, pageTitle: pageTitle, pageType: Constants.SCREEN_TRACKING_PAGE_GROUP), timerType: .custom, isGroupedTimer: true)
         } else {
-            self.timer = BlueTriangle.startTimer(page:Page(pageName: pageName, pageType: Constants.SCREEN_TRACKING_TRAFFIC_SEGMENT))
+            self.timer = BlueTriangle.startTimer(page:Page(pageName: pageName, pageType: Constants.SCREEN_TRACKING_PAGE_GROUP))
         }
     }
     
